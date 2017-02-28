12f01e1682e1b0aec6039d202831045bf9ebfb33
Erica Layton <3rica.layton@gmail.com>
2017-02-27 16:46:55 -0800

    movement is workiorking in AR and VR

---
+++ Assets/Resources/Scripts/IncomingVoiceAR.cs
@@ -0,0 +1,15 @@
+﻿using Photon;
+using UnityEngine;
+
+public class IncomingVoiceAR : PunBehaviour {
+	private AudioSource source;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
